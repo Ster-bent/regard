e33de48394a9ab3495775faf03c0291ebb26d801
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-02-12 16:16:08 +0000

    add js test

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 alert("Oops");
-var animA = document.getElementById("animA");
-
-const button = document.querySelector('.curver') 
+var curver = document.getElementById("curver");
  
-button.addEventListener('click', e => { 
-	alert('I have been clicked'); 
-    for (var i = 0; i <= 100; i+1) {
-        animA.style.borderRadius = i;
-    }
-}) 
+curver.addEventListener("click", myFunction);
 
+function myFunction() {
+    alert("test");
+}
