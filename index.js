129d7672f3bb27ef441d7471eb182d1baa3319c1
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-02-15 15:41:25 +0000

    fix

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-alert("1");
-var animA = document.getElementById("animA");
-var body = document.getElementById("body")
 var curver = document.getElementById("curver");
  
 curver.addEventListener("click", myFunction);
 
 function myFunction() {
     alert("ran")
-    body.style.backgroundColor = "blue";
     }
