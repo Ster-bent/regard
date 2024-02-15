e1711cc2d5a01eb4bbe1f980ce763809579c5e31
Sterbent <104021908+Ster-bent@users.noreply.github.com>
2024-02-15 15:39:22 +0000

    d

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-alert("Oops");
+alert("1");
 var animA = document.getElementById("animA");
+var body = document.getElementById("body")
 var curver = document.getElementById("curver");
  
 curver.addEventListener("click", myFunction);
 
 function myFunction() {
     alert("ran")
-    animA.style.borderRadius = 100;
+    body.style.backgroundColor = "blue";
     }
